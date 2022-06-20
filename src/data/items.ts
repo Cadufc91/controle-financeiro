681770e5ae3c68b851a5dcd7f97b9ca71cf32dc0
Cadufc91 <fernandes.cadu@gmail.com>
2022-06-20 18:37:00 -0300

    Estrutura + types: item + category

---
+++ src/data/items.ts
@@ -0,0 +1,8 @@
+import { Item } from '../types/Item';
+
+export const items: Item[] = [
+    { date: new Date(2022, 5, 20), category: 'food', title: 'McDonalds', value: 41.90 },
+    { date: new Date(2022, 5, 11), category: 'food', title: 'Outback', value: 90.00 },
+    { date: new Date(2022, 5, 12), category: 'food', title: 'Koni', value: 40.00 },
+    { date: new Date(2022, 5, 13), category: 'food', title: 'McDonalds', value: 41.90 },
+]
